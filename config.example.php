a9f336fe0613052c08cf1e1ff3be2d2508d7b213
Paul Dinelle <pauldinelle@gmail.com>
2015-05-21 20:03:21 +0000

    Added example config.

---
+++ config.example.php
@@ -0,0 +1,19 @@
+<?php
+
+// General information.
+define('RPG_SERVER_ROOT', '/your/root/folder');
+
+// Slack Webhook.
+define('SLACK_WEBHOOK', "https://hooks.slack.com/services/XXXXXXXX");
+define('SLACK_TOKEN', "XXXXX");
+define('SLACK_BOT_USERNAME', 'RPG');
+define('SLACK_BOT_ICON', ':rpg:');
+define('SLACK_BOT_TOKEN', "XXXXXX");
+define('SLACK_BOT_PUBLIC_CHANNEL', 'some-slack-channel');
+//define('SLACK_API_TOKEN', "XXXXXX");
+
+// Database configuration.
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'XXXX');
+define('DB_USER', 'XXXX');
+define('DB_PASS', 'XXXX');
